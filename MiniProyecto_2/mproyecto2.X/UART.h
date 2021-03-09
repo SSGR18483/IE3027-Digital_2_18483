7166dd27dc20add5ac545c80ff77f1ca15aadbb6
SSGR18483 <71710291+SSGR18483@users.noreply.github.com>
2021-03-09 01:09:30 -0600

    archivos

---
+++ MiniProyecto_2/mproyecto2.X/UART.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   UART.h
+ * Author: galic
+ *
+ * Created on 6 de marzo de 2021, 04:23 PM
+ */
+
+#ifndef UART_H
+#define	UART_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* UART_H */
+
---
+++ MiniProyecto_2/mproyecto2.X/mini2.c
@@ -0,0 +1,13 @@
+/*
+ * File:   mini2.c
+ * Author: galic
+ *
+ * Created on 6 de marzo de 2021, 04:21 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
