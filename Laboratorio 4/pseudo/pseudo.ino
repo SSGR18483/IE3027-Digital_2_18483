10b13e54ffef3d686099b8680684a58fb16b9c26
SSGR18483 <71710291+SSGR18483@users.noreply.github.com>
2021-03-29 23:34:23 -0600

    declaraciones

--- Laboratorio 4/pseudo/pseudo.ino
+++ Laboratorio 4/pseudo/pseudo.ino
@@ -4,27 +4,73 @@
 #define LED RED_LED
 #define LED1 GREEN_LED
 #define LED2 BLUE_LED
-const int buttonPin1 = PUSH1;
-const int buttonPin2 = PUSH2;
+int buttonPin1 = PUSH1;
+int buttonPin2 = PUSH2;
 
 int val =0;
 int val1 = 0;
-int cont1=B00000001;
-int cont2=B00000001;
+int cont1=0;
+int cont2=0;
+//JUGADOR 1 PINES:
+int led1 = PB_5;
+int led2 = PB_0;
+int led3 = PB_1;
+int led4 = PE_4;
+int led5 = PE_5;
+int led6 = PB_4;
+int led7 = PA_5;
+int led8 = PA_6;
+int gan1 = PA_7;
+//JUGADOR 2 PINES:
+int led9 = PA_2;
+int led10 = PD_7;
+int led11 = PD_6;
+int led12 = PC_7;
+int led13 = PC_6;
+int led14 = PC_5;
+int led15 = PC_4;
+int led16 = PB_3;
+int gan2 = PB_2;
+
+void semaforo(void);
+void ganador(void);
+void p1(void);
+void p2(void);
+
+
 int cont3 = 0;
 
 void setup() {
-    Serial.begin(115200);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  pinMode(led9,  OUTPUT);
+  pinMode(led10, OUTPUT);
+  pinMode(led11, OUTPUT);
+  pinMode(led12, OUTPUT);
+  pinMode(led13, OUTPUT);
+  pinMode(led14, OUTPUT);
+  pinMode(led15, OUTPUT);
+  pinMode(led16, OUTPUT);
+  pinMode(gan1, OUTPUT);
+  pinMode(gan2, OUTPUT);
+
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(buttonPin1, INPUT_PULLUP);
   pinMode(buttonPin2, INPUT_PULLUP);
+  semaforo();
 }
 
-void iniciar(){
-      delay(100);
+void semaforo(void){
+    delay(100);
     digitalWrite(LED, HIGH);
     delay(1100);
     digitalWrite(LED1, HIGH);
@@ -33,36 +79,28 @@ void iniciar(){
     delay(1100);
     digitalWrite(LED1, LOW);
   }
-void loop() {
-  // put your main code here, to run repeatedly: 
-  val= digitalRead(buttonPin1);
-  delay(100);
-  val1= digitalRead(buttonPin2);
-  delay(100);
-  if (val ==LOW || val1 == LOW) {
-    if(cont3==0){
-    cont3++;
-  iniciar();
-  }}
- if(val==LOW){
-  delay(100);
-  if(val ==HIGH){
-      delay(100);
-      cont1<<1;}
-      }
- if(val1==LOW){
-  delay(100);
-  if(val ==HIGH){
-      delay(100);
-      cont2<<1;}
-      }    
+void ganador(void){
+  if(cont1>8){
+    digitalWrite(gan1, HIGH);
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    loop();
+    }
+  if(cont2>8){}
+}
+void p1(void){
+  }
+void p2(void){
+  }
 
-if(cont1 == 128){
-  digitalWrite(LED2, HIGH);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED, LOW);}
-if(cont2 == 128){
-    digitalWrite(LED, HIGH);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED1, LOW);}
+void loop() {
+  p1();
+  p2();
+  ganador();
 }
